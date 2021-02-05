fa3cc3d9940a169bd00edd001c70c37322da2ba4
molleweide <hjalmar.jakobsson@telia.com>
2021-02-05 23:21:05 +0100

    clean

--- internal/library/routing.lua
+++ internal/library/routing.lua
@@ -38,6 +38,51 @@ local div2 = '---------------------------------'
 --      - flip phase
 
 --////////////////////////////////////////////////////////////////////////
+--  PUBLIC | move to custom ???
+--/////////
+
+function routing.create(route_str)
+  log.clear()
+  local _
+  if route_str == nil then
+    -- if no route_str provided >> user prompt
+    _, route_str = reaper.GetUserInputs("ENTER ROUTE STRING:", 1, route_help_str, input_placeholder)
+  end
+  local rp = extractParamsFromString(route_str)
+  prepareRouteComponents(rp)
+end
+
+function routing.removeAllSends(tr)
+  removeAll(tr)
+end
+
+function routing.removeAllRecieves(tr)
+  removeAll(tr, 1)
+end
+
+function routing.removeAllBoth(tr)
+  removeAll(tr, 2)
+end
+
+-- refactor and pet back to log
+function routing.logRoutingInfoForSelectedTracks()
+  log.clear()
+  local log_t = ru.getSelectedTracksGUIDs()
+  for i = 1, #log_t do
+    local tr, tr_idx = ru.getTrackByGUID(log_t[i])
+    local _, current_name = reaper.GetTrackName(tr)
+    log.user('\n\n'..div..'\n:: routes for track #' .. tr_idx+1 .. ' `' .. current_name .. '`:')
+
+    log.user('\n\tSENDs:')
+    logRoutesByCategory(tr, rc.flags.CAT_SEND)
+    log.user('\n\tRECIEVEs:')
+    logRoutesByCategory(tr, rc.flags.CAT_REC)
+    log.user('\n\tHARDWARE:')
+    logRoutesByCategory(tr, rc.flags.CAT_HW)
+  end
+end
+
+--////////////////////////////////////////////////////////////////////////
 --  UTILS
 --/////////
 
@@ -56,7 +101,6 @@ function getMatchedTrackGUIDs(search_name)
   return t
 end
 
--- mv to utils
 -- this function alse is defined in syntax/syntax
 function getStringSplitPattern(pString, pPattern)
   local Table = {}  -- NOTE: use {n = 0} in Lua-5.0
@@ -76,15 +120,10 @@ function getStringSplitPattern(pString, pPattern)
   end
   return Table
 end
+
 --//////////////////////////////////////////////////////////////////////
---  MIDI FLAGS
+--  MIDI FLAGS | move to midi utils
 --//////////////
---
---
---  the get functions return the `normal` decimal value
---  of the midi flag, eg src ch 1, dest ch 6, etc..
---
---  the input is the output from `I_MIDIFLAGS`
 
 --  GET FIRST 5 BITS
 function get_send_flags_src(flags) return flags & ((1 << 5)- 1) end
@@ -96,8 +135,8 @@ function get_send_flags_dest(flags) return flags >> 5 end
 function create_send_flags(src_ch, dest_ch) return (dest_ch << 5) | src_ch end
 
 --//////////////////////////////////////////////////////////////////////
---  LOGGING
---///////////
+--  ROUTE STATE LOGGING
+--///////////////////////
 
 -- hardware not working....
 function getOtherTrack(tr, cat, si)
@@ -111,9 +150,6 @@ function getOtherTrack(tr, cat, si)
   return other_tr, other_tr_idx
 end
 
--- TODO
---
--- if no audio / midi >>> don't show audio/midi
 function logRoutesByCategory(tr, cat)
   local num_sends = reaper.GetTrackNumSends(tr, cat)
   if num_sends == 0 then
@@ -164,69 +200,9 @@ function logRoutesByCategory(tr, cat)
   end
 end
 
-function routing.logRoutingInfoForSelectedTracks()
-  log.clear()
-  local log_t = ru.getSelectedTracksGUIDs()
-  for i = 1, #log_t do
-    local tr, tr_idx = ru.getTrackByGUID(log_t[i])
-    local _, current_name = reaper.GetTrackName(tr)
-    log.user('\n\n'..div..'\n:: routes for track #' .. tr_idx+1 .. ' `' .. current_name .. '`:')
-
-    log.user('\n\tSENDs:')
-    logRoutesByCategory(tr, rc.flags.CAT_SEND)
-    log.user('\n\tRECIEVEs:')
-    logRoutesByCategory(tr, rc.flags.CAT_REC)
-    log.user('\n\tHARDWARE:')
-    logRoutesByCategory(tr, rc.flags.CAT_HW)
-  end
-end
-
 --//////////////////////////////////////////////////////////////////////
---  CREATE ROUTE
---////////////////
--- 0 = none
--- 1 = only audio
--- 2 = only midi
--- 3 = both
-function getPrevRouteState(src_tr, dest_tr, rp)
-  rp.prev = 0
-  for si=0,  reaper.GetTrackNumSends( src_tr, 0 ) do
-    local dest_tr_check = reaper.BR_GetMediaTrackSendInfo_Track( src_tr, 0, si, 1 )
-    if dest_tr_check == dest_tr then
-      local prev_src_midi_flags = reaper.GetTrackSendInfo_Value(src_tr, 0, si, 'I_MIDIFLAGS')
-      local prev_src_audio_ch = reaper.GetTrackSendInfo_Value(src_tr, 0, si, 'I_SRCCHAN')
-      -- both audio and midi
-      local retval = 3
-      rp.prev = 3
-
-      local no_midi = prev_src_midi_flags == rc.flags.MIDI_OFF
-      local no_audio = prev_src_audio_ch ==  rc.flags.AUDIO_SRC_OFF
-      -- only audio = 1
-      if no_midi then
-        rp.prev = 1
-      end
-      -- only midi = 2
-      if no_audio then
-        rp.prev = 2
-      end
-      -- log.user(tostring(si) .. '   ' .. tostring(no_midi) .. '   ' .. tostring(no_audio) .. '   ' .. tostring(retval))
-      return rp, si
-    end
-  end
-  return rp
-end
-
-function routing.create(route_str)
-  log.clear()
-  local _
-  if route_str == nil then
-    --
-    -- if no route_str provided >> user prompt
-    _, route_str = reaper.GetUserInputs("ENTER ROUTE STRING:", 1, route_help_str, input_placeholder)
-  end
-  local rp = extractParamsFromString(route_str)
-  prepareRouteComponents(rp)
-end
+--  EXTRACT ROUTE PARAMS
+--///////////////////////
 
 function removeEnclosureFromString(str, encl_type)
   for r in str:gmatch ("%b"..encl_type) do
@@ -359,6 +335,97 @@ function extractParamsFromString(str)
   return rp
 end
 
+--//////////////////////////////////////////////////////////////////////
+--  GET ROUTE STATE
+--////////////////
+
+function getPrevRouteState(src_tr, dest_tr, rp)
+  rp.prev = 0
+  for si=0,  reaper.GetTrackNumSends( src_tr, 0 ) do
+    local dest_tr_check = reaper.BR_GetMediaTrackSendInfo_Track( src_tr, 0, si, 1 )
+    if dest_tr_check == dest_tr then
+      local prev_src_midi_flags = reaper.GetTrackSendInfo_Value(src_tr, 0, si, 'I_MIDIFLAGS')
+      local prev_src_audio_ch = reaper.GetTrackSendInfo_Value(src_tr, 0, si, 'I_SRCCHAN')
+      local retval = 3 -- both audio and midi
+      rp.prev = 3
+      local no_midi = prev_src_midi_flags == rc.flags.MIDI_OFF
+      local no_audio = prev_src_audio_ch ==  rc.flags.AUDIO_SRC_OFF
+      -- only audio = 1
+      if no_midi then rp.prev = 1 end
+      -- only midi = 2
+      if no_audio then rp.prev = 2 end
+      return rp, si
+    end
+  end
+  return rp
+end
+
+function getNextRouteState(rp, check_str)
+  if (rp.new_params['a'] ~= nil and rp.new_params['m'] == nil) or (rp.new_params['a'] == nil and rp.new_params['m'] == nil) then
+    rp.next = 1 -- add only audio (default if no a/m input)
+  end
+  if rp.new_params['a'] == nil and rp.new_params['m'] ~= nil then
+    rp.next = 2 -- add only midi
+  end
+  if rp.new_params['a'] == nil and rp.new_params['m'] ~= nil then
+    rp.next = 3 -- add both
+  end
+  return rp -- we should never arrive here i think since default always is add audio send
+end
+
+--////////////////////////////////////////////////////////////////////
+--  REMOVE ROUTES
+--/////////////////
+
+function deleteRouteIfEmpty(src_tr, rid)
+  local i_src_ch = reaper.GetTrackSendInfo_Value(src_tr, 0, rid, 'I_SRCCHAN')
+  local i_src_midi = reaper.GetTrackSendInfo_Value(src_tr, 0, rid, 'I_MIDIFLAGS')
+  if i_src_ch == rc.flags.AUDIO_SRC_OFF and i_src_midi == rc.flags.MIDI_OFF then
+    log.user('::delete send ' .. rid .. '::')
+    removeSingle(src_tr, 0, rid)
+  end
+end
+
+function removeSingle(tr, cat, sendidx)
+  local ret = reaper.RemoveTrackSend(tr, cat, sendidx)
+end
+
+function deleteByCategory(tr, cat)
+  local num_sends = reaper.GetTrackNumSends(tr, cat)
+  -- if num_sends == 0 then return end
+  while(num_sends > 0) do
+    for si=0, num_sends-1 do
+      local rm = reaper.RemoveTrackSend(tr, cat, si)
+    end
+    num_sends = reaper.GetTrackNumSends(tr, cat)
+  end
+end
+
+function removeAll(tr, kind)
+  log.clear()
+  log.user('removeAll')
+  local target_t = {}
+  target_t[#target_t] = tr
+  if tr == nil or tr == false then target_t = ru.getSelectedTracksGUIDs() end -- get table of src tracks
+  -- FOR EACH TRACK WE WANT TO TARGET
+  for i = 1, #target_t do
+    local tr = ru.getTrackByGUID(target_t[i])
+    if kind == nil or kind == 0 then
+      deleteByCategory(tr, rc.flags.CAT_SEND)
+    elseif kind == 1 then
+      deleteByCategory(tr, rc.flags.CAT_REC)
+    elseif kind == 2 then
+      deleteByCategory(tr, rc.flags.CAT_SEND)
+      deleteByCategory(tr, rc.flags.CAT_REC)
+    end
+  end
+  return true
+end
+
+--///////////////////////////////////////////////////////////////////////
+--  UPDATE ROUTE STATE
+--//////////////////////
+
 function prepareRouteComponents(rp)
   log.user('prepareRouteComponents')
   local src_t
@@ -399,111 +466,19 @@ function prepareRouteComponents(rp)
   if answer ~= "y" then return end
 
   -- EXECUTE / UPDATE ROUTING STATE
-  routing.createRoutesLoop(rp, src_t, dest_tr)
+  createRoutesLoop(rp, src_t, dest_tr)
 end
 
-function getNextRouteState(rp, check_str)
-  if (rp.new_params['a'] ~= nil and rp.new_params['m'] == nil) or (rp.new_params['a'] == nil and rp.new_params['m'] == nil) then
-    rp.next = 1 -- add only audio (default if no a/m input)
-  end
-  if rp.new_params['a'] == nil and rp.new_params['m'] ~= nil then
-    rp.next = 2 -- add only midi
-  end
-  if rp.new_params['a'] == nil and rp.new_params['m'] ~= nil then
-    rp.next = 3 -- add both
-  end
-
-  -- we should never arrive here i think since default always is add audio send
-  return rp
-end
-
-function routing.createRoutesLoop(rp, src_t, dest_tr)
+function createRoutesLoop(rp, src_t, dest_tr)
   for i = 1, #src_t do
     local src_tr =  reaper.BR_GetMediaTrackByGUID( 0, src_t[i] )
-    -- why are these two put here. can they be put inside of  createisingle funcs??
-    -- local src_tr_ch = reaper.GetMediaTrackInfo_Value( src_tr, 'I_NCHAN')
-    -- local dest_tr_ch = incrementDestChanToSrc(dest_tr, src_tr_ch)
     local rp, rid = getPrevRouteState(src_tr, dest_tr, rp)
     local rp = getNextRouteState(rp)
     if rp.prev == 0 then rid = reaper.CreateTrackSend(src_tr, dest_tr) end
-
     updateRouteState_Track(src_tr, rp, rid)
-
-    -- DELETE SEND IF EMPTY
-    local i_src_ch = reaper.GetTrackSendInfo_Value(src_tr, 0, rid, 'I_SRCCHAN')
-    local i_src_midi = reaper.GetTrackSendInfo_Value(src_tr, 0, rid, 'I_MIDIFLAGS')
-    if i_src_ch == rc.flags.AUDIO_SRC_OFF and i_src_midi == rc.flags.MIDI_OFF then
-      log.user('::delete send ' .. rid .. '::')
-      removeSingle(src_tr, 0, rid)
-    end
+    deleteRouteIfEmpty(src_tr, rid)
   end
   log.user(format.block(rp))
-end
-
---////////////////////////////////////////////////////////////////////
---  REMOVE ROUTES
---/////////////////
-
-function removeSingle(tr, cat, sendidx)
-  local ret = reaper.RemoveTrackSend(tr, cat, sendidx)
-end
-
--- all these 3 functions can be refactored into one
--- tiny function if I create a good pattern for handling
--- the removal/disabling of a send/recieve.
-function routing.removeAllSends(tr)
-  removeAll(tr)
-end
-
-function routing.removeAllRecieves(tr)
-  removeAll(tr, 1)
-end
-
-function routing.removeAllBoth(tr)
-  removeAll(tr, 2)
-end
-
-function deleteByCategory(tr, cat)
-  local num_sends = reaper.GetTrackNumSends(tr, cat)
-  -- if num_sends == 0 then return end
-  while(num_sends > 0) do
-    for si=0, num_sends-1 do
-      local rm = reaper.RemoveTrackSend(tr, cat, si)
-    end
-    num_sends = reaper.GetTrackNumSends(tr, cat)
-  end
-end
-
-function removeAll(tr, kind)
-  log.clear()
-  log.user('removeAll')
-  local target_t = {}
-  target_t[#target_t] = tr
-  if tr == nil or tr == false then target_t = ru.getSelectedTracksGUIDs() end -- get table of src tracks
-  -- FOR EACH TRACK WE WANT TO TARGET
-  for i = 1, #target_t do
-    local tr = ru.getTrackByGUID(target_t[i])
-    if kind == nil or kind == 0 then
-      deleteByCategory(tr, rc.flags.CAT_SEND)
-    elseif kind == 1 then
-      deleteByCategory(tr, rc.flags.CAT_REC)
-    elseif kind == 2 then
-      deleteByCategory(tr, rc.flags.CAT_SEND)
-      deleteByCategory(tr, rc.flags.CAT_REC)
-    end
-  end
-  return true
-end
-
---///////////////////////////////////////////////////////////////////////
---  HANDLE SINGLE TRACK
---///////////////////////
-
-function incrementDestChanToSrc(dest_tr, src_tr_ch)
-  log.user('incrementDestChanToSrc')
-  local dest_tr_ch = reaper.GetMediaTrackInfo_Value( dest_tr, 'I_NCHAN')
-  if dest_tr_ch < src_tr_ch then reaper.SetMediaTrackInfo_Value( dest_tr, 'I_NCHAN', src_tr_ch ) end
-  return dest_tr_ch
 end
 
 function updateRouteState_Track(src_tr, rp, rid)
@@ -537,13 +512,12 @@ end
 --
 -- remove this function
 
+-- replace this function in Syntax with my new route_str
 function routing.createSingleMIDISend(src_tr,dest_tr,dest_chan)
   log.user('createSingleMIDISend')
   local is_exist = getPrevRouteState(src_tr, dest_tr)
 
-  -- TODO
-  -- if dest_chan == nil then set to 0 (ALL)
-  --
+  -- TODO if dest_chan == nil then set to 0 (ALL)
 
   log.user('midi sends exists ???????  : ' .. tostring(is_exist))
   if not is_exist then
@@ -552,6 +526,13 @@ function routing.createSingleMIDISend(src_tr,dest_tr,dest_chan)
     reaper.SetTrackSendInfo_Value(src_tr, rc.flags.CAT_SEND, midi_send_id, "I_MIDIFLAGS", new_midi_flags) -- set midi_flags on reference
     reaper.SetTrackSendInfo_Value(src_tr, rc.flags.CAT_SEND, midi_send_id, "I_SRCCHAN", rc.flags.AUDIO_SRC_OFF)
   end
+end
+
+-- rm ????????
+function incrementDestChanToSrc(dest_tr, src_tr_ch)
+  local dest_tr_ch = reaper.GetMediaTrackInfo_Value( dest_tr, 'I_NCHAN')
+  if dest_tr_ch < src_tr_ch then reaper.SetMediaTrackInfo_Value( dest_tr, 'I_NCHAN', src_tr_ch ) end
+  return dest_tr_ch
 end
 
 function preventRouteFeedback()
