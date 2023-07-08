3c2a91db77e1e3cbdcdee44d1b261c1b084fa968
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2023-07-08 01:10:34 -0400

    save

--- scripts/generator.rb
+++ scripts/generator.rb
@@ -2,7 +2,7 @@
 
 require('./scripts/key_definitions')
 
-# TODO: rename script_id -> action_command_id !!
+# TODO: rename script_id -> action_command_id !!!!!
 
 class Generator
   def initialize(root_dir_path, keymap_path, key_script_dir)
