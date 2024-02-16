9268304b3febb1f769735ce97426fbdde023eaa0
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2024-02-16 10:04:55 -0500

    save

--- internal/library/apply_music_transform.lua
+++ internal/library/apply_music_transform.lua
@@ -32,27 +32,56 @@ end
 local amt = {}
 
 amt.apply_patterns_to_sel_tracks = function(opts, t_sel_trks, main_input_str)
-	-- Get music data from string
-	local ok, t_final_rendered_notes = midi.parse_and_render_midi_notes_block_from_string(main_input_str)
+	local custom_targets = opts.targets or {}
+	local target_tracks, target_ranges
+
+  --
+  -- GET MUSIC DATA FROM STRING
+  --
+  -- TODO: add good defaults if not string is provided
+  --
+
+	local ok, t_final_rendered_notes = midi.parse_and_render_midi_notes_block_from_string(opts.prompt_str)
 	if not ok then
 		return false
 	end
 
-	local custom_targets = opts.targets or {}
+	--
+	-- COMPUTE TARGET TRACKS
+	--
 
 	if custom_targets.tracks then
+		target_tracks = custom_targets.tracks
+	else
+		local focused_track_objects, _, context = lib_tr.get_focused_track_objects()
+		target_tracks = focused_track_objects
 	end
+
+	--
+	-- COMPUTE TIMELINE RANGES
+	--
+
+	-- TODO: log ranges etc.
 
 	if custom_targets.regions then
+		-- TODO: extract ranges and put in target_ranges table?
+		target_regions = custom_targets.regions
+	else
+		-- TODO: get range from timeline OR motion/selection
+		local cursor_info = tl.get_cursor_info()
 	end
 
-	local cursor_info = tl.get_cursor_info()
+	--
+	-- APPLY MUSIC TRANSFORM LOOP
+	--
 
+	-- 1. for each track node
+	-- 2. for each region
 
-	for _, trnode in ipairs(t_sel_trks) do
-		local target_item = check_if_item_exists_or_create(trnode.tr, cursor_info.msr.start, cursor_info.msr._end)
-		apply_music_transform_hooks(trnode, target_item, t_final_rendered_notes)
-	end
+	-- for _, trnode in ipairs(target_tracks) do
+	-- 	local target_item = check_if_item_exists_or_create(trnode.tr, cursor_info.msr.start, cursor_info.msr._end)
+	-- 	apply_music_transform_hooks(trnode, target_item, t_final_rendered_notes)
+	-- end
 
 	return true
 end
