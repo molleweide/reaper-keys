4cbef5fae432a52e18e62f7ac640cd2a6a88a782
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2024-04-14 03:30:28 -0400

    save

---
+++ definitions/song_test.lua
@@ -0,0 +1,24 @@
+return {
+    song1 = {
+        "intro",
+        "verse 1",
+        "chorus 1st",
+        "verse 2",
+        "chorus 2nd",
+        "bridge",
+        "chorus final",
+        "outro",
+    },
+  song2 = {
+    "on",
+    "off",
+    "on",
+    "off",
+    "on",
+    "off",
+    "on",
+    "off",
+    "on",
+    "off",
+  }
+}
